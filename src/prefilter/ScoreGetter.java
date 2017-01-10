4cf1ff44f22171f1f9a90b2de9708d56bb24cab2
RamsesXVII <mattia.iodice3@gmail.com>
2017-01-10 13:47:46 +0100

    aggiungo versione aggiornata del prefiltro e filtro relazionale

--- src/prefilter/ScoreGetter.java
+++ src/prefilter/ScoreGetter.java
@@ -2,39 +2,82 @@ package prefilter;
 
 
 public class ScoreGetter {
-	
-	private String[]listId={"such as","including","i.e.","like","for example","e.g."};
+	private double threashold=1.4;
 
-	private String[]numbersSymbol={"1","2","3","4","5","6","7","8","9","10"};
+	private String[]listId={"such as","including","i\\.e\\.","like","both","for example","e\\.g\\."};
+
+	private String[]numbersSymbol={" 1 "," 2 "," 3 "," 4 "," 5 "," 6 "," 7 "," 8 "," 9 "," 10 "};
 	private String[]numberLiterals={"one","two","three","four","five","six","seven","eight","nine","ten"};
-	
+
 	private String[]listQuantifiers={"some","many","a lot","a lots","different","any","plenty","few"};
 
 	private String entityRegex=".*\\[\\[[a-zA-Z0-9_()/,\\.&-]{0,70}\\|m.[a-zA-Z0-9_]*\\]\\].*";
 	private String notEntityRegex="[a-zA-Z0-9_]{0,20}";
+	
+	//secondo me qui una almeno deve essere entià
 	private String listThreeElementsRegex="((("+entityRegex+"|"+notEntityRegex+") ){1,5}, )+(("+entityRegex+"|"+notEntityRegex+") ){1,5}and( ("+entityRegex+"|"+notEntityRegex+")){1,5}";
 
+	//secondo me qui una almeno deve essere entià
 	private String containsColonFollowedByListRegex=".*: ((("+entityRegex+"|"+notEntityRegex+") ){1,5}, )*(("+entityRegex+"|"+notEntityRegex+") ){1,5}and( ("+entityRegex+"|"+notEntityRegex+")){1,5}.*";
+
+	private String listThreeElementsRegex1=".*: ((("+entityRegex+"|"+notEntityRegex+") ){1,5}, )*(("+entityRegex+"|"+notEntityRegex+") ){1,5}and( ("+entityRegex+"|"+notEntityRegex+")){1,5}.*";
+	private String listThreeElementsRegex2=".*: ((("+entityRegex+"|"+notEntityRegex+") ){1,5}, )*(("+entityRegex+") ){1,5}and( ("+entityRegex+"|"+notEntityRegex+")){1,5}.*";
+	private String listThreeElementsRegex3=".*: ((("+entityRegex+"|"+notEntityRegex+") ){1,5}, )*(("+entityRegex+"|"+notEntityRegex+") ){1,5}and( ("+entityRegex+")){1,5}.*";
 
 
 
 	public double getScore(String phrase){
+/*		
+		System.out.println(this.containsColonFollowedByListPattern(phrase)+" colonFollowedByListPatern");
+		System.out.println(this.containsColon(phrase)+" containsColon");
+		System.out.println(this.containsListIdFollwedByEntity(phrase)+" containsListIdFollowedEntity");
+		System.out.println(this.containsListOfThreeElementsPattern(phrase)+" containsListOfThreeElementsPattern");
+		System.out.println(this.containsQuantifiers(phrase)+" containsQuantifiers");
+		System.out.println(this.containsNumberCommaPattern(phrase)+" containsNumberCommaPattern");
+		System.out.println(this.getEntityCount(phrase)+" getEntityCount");
+		System.out.println(this.getCommasCount(phrase)+" getCommasCount"); */
+		 
 		double globalVal=0;
-		
-		globalVal+=this.containsColonFollowedByListPattern(phrase)*(0.8);
-		globalVal+=this.containsColon(phrase)*(0.4);
-		globalVal+=this.containsListIdFollwedByEntity(phrase)*(0.8); //tiene conto di quella che matcha, quindi devono essercene almeno 2 complessivamente
-		globalVal+=this.containsListOfThreeElementsPattern(phrase)*(0.8);
-		globalVal+=this.containsQuantifiers(phrase)*(0.4);
-		globalVal+=this.containsNumberCommaPattern(phrase)*(0.4);
-		globalVal+=this.getEntityCount(phrase)*(0.1);
-		globalVal+=this.getCommasCount(phrase)*(0.05);
+
+		if(phrase.matches(this.entityRegex+this.entityRegex)){
+			globalVal+=this.containsColonFollowedByListPattern(phrase)*(0.8);
+			globalVal+=this.containsListIdFollwedByEntity(phrase)*(0.8); //tiene conto di quella che matcha, quindi devono essercene almeno 2 complessivamente
+
+			if(globalVal>=threashold)
+				return globalVal;
+
+			globalVal+=this.containsQuantifiers(phrase)*(0.4);
+			if(globalVal>=threashold)
+				return globalVal;
+
+			globalVal+=this.containsColon(phrase)*(0.4);
+			if(globalVal>=threashold)
+				return globalVal;
+
+			globalVal+=this.containsNumberCommaPattern(phrase)*(0.4);
+			if(globalVal>=threashold)
+				return globalVal;
+
+			globalVal+=this.getEntityCount(phrase)*(0.1);
+			if(globalVal>=threashold)
+				return globalVal;
+
+			globalVal+=this.getCommasCount(phrase)*(0.05);
+			if(globalVal>=threashold)
+				return globalVal;
+
+			globalVal+=this.containsListOfThreeElementsPattern(phrase)*(0.8);
+
+		}
+
 
 		return globalVal;
 	}
 
 	public int containsColonFollowedByListPattern(String phrase){
-		if(phrase.matches(containsColonFollowedByListRegex))
+		//TODO 		String phrase="Besides rowing  , [[Clube_de_Regatas_do_Flamengo|m.019lty]] also plays an active role in several [[Summer_Olympics|m.06vvk]] sports , such as : Artistic gymnastica , athletics  , basketball  , judo , swimming  , volleyball and water polo .";
+
+		if(phrase.matches(containsColonFollowedByListRegex)&&phrase.matches(".* :"+this.entityRegex))
 			return 1;
 		else 
 			return 0;
@@ -49,7 +92,7 @@ public class ScoreGetter {
 	}
 
 	public int containsListIdFollwedByEntity(String phrase) {
-		 String listIdRegex;
+		String listIdRegex;
 
 		for(String s :listId){
 			listIdRegex=".*"+s+this.entityRegex;
@@ -61,13 +104,16 @@ public class ScoreGetter {
 
 	public int containsListOfThreeElementsPattern(String phrase){
 
-		if(phrase.matches(this.listThreeElementsRegex))
-			return 1;
+
+		if(phrase.matches(this.listThreeElementsRegex1)||
+				phrase.matches(this.listThreeElementsRegex2)||
+				phrase.matches(this.listThreeElementsRegex3)){
+			return 1;}
 		else
 			return 0;
 
 	}
-	
+
 	public int containsQuantifiers(String phrase){
 		for(String quantifiers:this.listQuantifiers){
 			if (phrase.contains(quantifiers))
@@ -78,13 +124,14 @@ public class ScoreGetter {
 
 	public int containsNumberCommaPattern(String phrase){
 
+		//TODO
 		for(int i=0;i<10;i++){
-			if (phrase.contains(this.numberLiterals[i])||phrase.contains(this.numbersSymbol[i])){
+			if (phrase.contains(" "+this.numberLiterals[i]+" ")||phrase.contains(this.numbersSymbol[i])){
 				if(this.getCommasCount(phrase)>=(i-1)) //la numerazione parte da 0
 					return 1;
-				
+
 			}
-				
+
 		}
 		return 0;
 	}
@@ -96,9 +143,9 @@ public class ScoreGetter {
 				counter++;
 		}
 		return counter/2;
-		
+
 	}
-	
+
 	public int getCommasCount(String phrase){
 		int counter=0;
 		for(int i=0;i<phrase.length();i++){
@@ -107,5 +154,13 @@ public class ScoreGetter {
 		}
 		return counter;
 	}
-	
+
+	public double getThreashold() {
+		return threashold;
+	}
+
+	public void setThreashold(double threashold) {
+		this.threashold = threashold;
+	}
+
 }
---
+++ src/relationIdentifier/main.java
@@ -0,0 +1,14 @@
+package relationIdentifier;
+
+import relationIdentifier.RelationalIdentifier;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		RelationalIdentifier ri= new RelationalIdentifier();
+
+		ri.isRelational("is a supporter of");
+	}
+
+}
