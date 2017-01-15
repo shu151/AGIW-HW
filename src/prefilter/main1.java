80a88455134b3ca207fc3217e61042d6628a2e67
Mattia Iodice <mattia.iodice3@gmail.com>
2017-01-15 11:57:47 +0100

    Update main1.java

--- src/prefilter/main1.java
+++ src/prefilter/main1.java
@@ -5,7 +5,7 @@ public class main1 {
 	public static void main(String[] args) {
 		
 		String pathToInputFile="evaluationcorpus.tsv";
-		boolean useMetrics=true; //le frasi del file di input devono essere etichettate!!! (tab Y otab N)
+		boolean useMetrics=true; //se impostato a true allora le frasi del file di input devono essere etichettate!!! (tab Y otab N)
 		
 		ParallelFilter pf = new ParallelFilter();
 		pf.filterSentences(pathToInputFile,useMetrics);
